3c939e2a851eb318ada320ba5fe4c8b552836898
Isha Shah <isha.shah@Jolera.com>
2025-08-06 14:51:14 -0400

    Initial commit of JoleraONE project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JoleraONEDashboard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
